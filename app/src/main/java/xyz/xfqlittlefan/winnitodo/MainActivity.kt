0c64aa6a83152d4a63577e0c68440c1249f795b4
xfqwdsj <xfqwdsj@qq.com>
2023-09-22 23:52:00 +0800

    init

---
+++ app/src/main/java/xyz/xfqlittlefan/winnitodo/MainActivity.kt
@@ -0,0 +1,42 @@
+package xyz.xfqlittlefan.winnitodo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import xyz.xfqlittlefan.winnitodo.ui.theme.WinniTODOTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WinniTODOTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = {
+                            Text(text = stringResource(id = R.string.app_name))
+                        })
+                    },
+                    bottomBar = {},
+                ) { contentPadding ->
+                    Surface(
+                        modifier = Modifier
+                            .padding(contentPadding)
+                            .fillMaxSize(),
+                    ) {
+                        Text(text = "Hello World!")
+                    }
+                }
+            }
+        }
+    }
+}
